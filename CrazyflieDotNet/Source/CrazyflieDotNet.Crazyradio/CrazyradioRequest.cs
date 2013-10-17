ddb5b70552380ce7e68c37d053dd2b5e495da125
ckarcz <ckarcz@gmail.com>
2013-10-16 23:17:53 -0400

    Updated build folders for debug and release
    Updated build folders for debug and release and fixed license headers

--- CrazyflieDotNet/Source/CrazyflieDotNet.Crazyradio/CrazyradioRequest.cs
+++ CrazyflieDotNet/Source/CrazyflieDotNet.Crazyradio/CrazyradioRequest.cs
@@ -1,15 +1,15 @@
 ﻿/* 
- *										 _ _  _     
- *			   ____ ___  ___  __________(_|_)(_)____
- *			  / __ `__ \/ _ \/ ___/ ___/ / _ \/ ___/
- *			 / / / / / /  __(__  |__  ) /  __/ /    
- *			/_/ /_/ /_/\___/____/____/_/\___/_/  
+ *						 _ _  _     
+ *		       ____ ___  ___  __________(_|_)(_)____
+ *		      / __ `__ \/ _ \/ ___/ ___/ / _ \/ ___/
+ *		     / / / / / /  __(__  |__  ) /  __/ /    
+ *		    /_/ /_/ /_/\___/____/____/_/\___/_/  
  *
- *				Copyright 2013 - http://www.messier.com
+ *		    Copyright 2013 - http://www.messier.com
  *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *	This Source Code Form is subject to the terms of the Mozilla Public
+ *	License, v. 2.0. If a copy of the MPL was not distributed with this
+ *	file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
 namespace CrazyflieDotNet.Crazyradio
